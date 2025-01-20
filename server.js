0d6af002521e0f788ff53afc839306fdda122b2c
Ionut-Vidrighin <ionut.vidrighin@gmail.com>
2025-01-20 20:53:19 +0200

    renaming file

---
+++ server.js
@@ -0,0 +1,36 @@
+const IPFS = require('ipfs');
+
+const startRelayNode = async () => {
+  try {
+    const node = await IPFS.create({
+      repo: './ipfs-repo',
+      config: {
+        Addresses: {
+          Swarm: [
+            '/ip4/0.0.0.0/tcp/4001',
+            '/ip4/0.0.0.0/udp/4001/quic',
+            '/ip4/0.0.0.0/udp/4001/webrtc-direct',
+          ],
+          API: '/ip4/0.0.0.0/tcp/5001',
+          Gateway: '/ip4/0.0.0.0/tcp/8080',
+        },
+        Swarm: {
+          RelayService: {
+            Enabled: true, // Enable relay functionality
+          },
+        },
+      },
+    });
+
+    console.log('IPFS relay node is running...');
+    console.log('Node ID:', (await node.id()).id);
+
+    // Prevent the script from exiting
+    process.stdin.resume();
+  } catch (error) {
+    console.error('Error starting IPFS relay node:', error);
+    process.exit(1);
+  }
+};
+
+startRelayNode();
